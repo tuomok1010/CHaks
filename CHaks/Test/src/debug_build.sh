84fa70db24c21cc8cff5e81b56db5cc3842bfd3f
tuomok1010 <tuomok1010@gmail.com>
2022-01-05 07:32:50 -0500

    testing stuff with libnetfilter queue in Test project

--- CHaks/Test/src/debug_build.sh
+++ CHaks/Test/src/debug_build.sh
@@ -7,5 +7,5 @@ export LD_LIBRARY_PATH=$LibPath:$LD_LIBRARY_PATH
 
 
 cd ../build/debug
-g++ -L$LibPath -Wl,-rpath,$LibPath -Wall -D DEBUG_BUILD -o Test $Code -I$IncludePath -lpacketcraft
+g++ -L$LibPath -Wl,-rpath,$LibPath -Wall -D DEBUG_BUILD -o Test $Code -I$IncludePath -lpacketcraft -lnetfilter_queue
 cd ../../src
